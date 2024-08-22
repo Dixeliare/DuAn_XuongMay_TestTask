297c5cea537a91b768c8e79016e5d9173d5eec02
Dixeliare <daoxuanlong492004@gmail.com>
2024-08-22 18:15:21 +0700

    CRUD Product , CRUD Category

---
+++ XuongMayNhom8.Repositories/Repositories/CategoryRepository/CategoryRepository.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace XuongMayNhom8.Repositories.Repositories.CategoryRepository
+{
+    internal class CategoryRepository
+    {
+    }
+}
---
+++ XuongMayNhom8.Services/Services/ProductService/IProductService.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace XuongMayNhom8.Services.Services.ProductService
+{
+    internal interface IProductService
+    {
+    }
+}
